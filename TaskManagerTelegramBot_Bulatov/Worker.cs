ac0cd1bdbc6cb48d226bc32805dd2b5894d6bea5
Chelovek <forstudkak@gmail.com>
2025-12-15 20:47:47 +0500

    Разработанный методы создания кнопок, и проверка на вводимую дату и время. Булатов

--- TaskManagerTelegramBot_Bulatov/Worker.cs
+++ TaskManagerTelegramBot_Bulatov/Worker.cs
@@ -1,7 +1,56 @@
+﻿using TaskManagerTelegramBot_Bulatov.Classes;
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+
 namespace TaskManagerTelegramBot_Bulatov
 {
     public class Worker : BackgroundService
     {
+        readonly string Token = "";
+        TelegramBotClient TelegramBotClient;
+        List<Users> Users = new List<Users>();
+        Timer Timer;
+        List<string> Messages = new List<string>()
+        {
+            "Здравствуйте! " +
+            "\nРады приветствовать вас в Telegram-боте «Напоминатор»! 😊  " +
+            "\nНаш бот создан для того, чтобы напоминать вам о важных событиях и мероприятиях. " +
+            "С ним вы точно не пропустите ничего важного! 💬  " +
+            "\nНе забудьте добавить бота в список своих контактов и настроить уведомления. " +
+            "Тогда вы всегда будете в курсе событий! 😊",
+
+            "Укажите дату и время напоминания в следующем формате:" +
+            "\n<i><b>12:51 26.01.2025</b>" +
+            "\nНапомни о том что я хотел сходить в магазин.</i>",
+
+            "Кажется, что-то не получилось." +
+            "Укажите дату и время напоминания в следующем формате:" +
+            "\n<i><b>12:51 26.01.2025</b>" +
+            "\nНапомни о том что я хотел сходить в магазин.</i>",
+            "",
+            "Задачи пользователя не найдены.",
+            "Событие удалено.",
+            "Все события удалены."
+            };
+        public bool CheckFormatDateTime(string value, out DateTime time)
+        {
+            return DateTime.TryParse(value, out time);
+        }
+        public static ReplyKeyboardMarkup GetButtons()
+        {
+            List<KeyboardButton> keyboardButtons = new List<KeyboardButton>();
+            keyboardButtons.Add(new KeyboardButton("Удалить все задачи"));
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>() { keyboardButtons }  
+            };
+        }
+        public static InlineKeyboardMarkup DeleteEvent(string Message)
+        {
+            List<InlineKeyboardButton> inlineKeyboards = new List<InlineKeyboardButton>();
+            inlineKeyboards.Add(new InlineKeyboardButton("Удалить", Message));
+            return new InlineKeyboardMarkup(inlineKeyboards);
+        }
         private readonly ILogger<Worker> _logger;
 
         public Worker(ILogger<Worker> logger)
