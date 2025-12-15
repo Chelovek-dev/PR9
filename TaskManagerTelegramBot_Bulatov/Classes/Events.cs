6cd401743edb1d5308134f40f8f4a0768d8a0e46
Chelovek <forstudkak@gmail.com>
2025-12-15 20:36:56 +0500

    Созданный классы для приложения. Булатов

---
+++ TaskManagerTelegramBot_Bulatov/Classes/Events.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagerTelegramBot_Bulatov.Classes
+{
+    public class Events
+    {
+        public DateTime TIme {  get; set; }
+        public string Message { get; set; }
+        public Events(DateTime time, string message)
+        {
+            TIme = time;
+            Message = message;
+        }
+    }
+}
---
+++ TaskManagerTelegramBot_Bulatov/Classes/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagerTelegramBot_Bulatov.Classes
+{
+    public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long idUser) 
+        {
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+    }
+}
